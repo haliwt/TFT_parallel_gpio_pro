4b60d6d2e398cf67f4eb1cfbd3907c6d172ce6e8
haliwt <jonewang@126.com>
2024-07-09 08:11:35 +0800

    update

--- Bsp/src/bsp_key_app.c
+++ Bsp/src/bsp_key_app.c
@@ -273,7 +273,7 @@ void DEC_Key_Fun(void)
 	          gpro_t.gTimer_pro_disp_temphum = 0; //don't display sensor realy temperature value .
 			 gctl_t.gSet_temperature_value--;
 			if( gctl_t.gSet_temperature_value<20)  gctl_t.gSet_temperature_value=40;
-	        if( gctl_t.gSet_temperature_value >40) gctl_t.gSet_temperature_value=40;
+	        if( gctl_t.gSet_temperature_value >40) gctl_t.gSet_temperature_value=20;//40
    
 			 gpro_t.gTimer_pro_set_tem_value_blink =0;
 			 gctl_t.gSet_temperature_value_item = disp_do_setting_ptc_value_item;
